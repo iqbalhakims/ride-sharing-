04caae9a7c0bfc40c2a836c993b26dc436b3bc58
Iqbal Fauzi <teamstac350@gmail.com>
2026-03-12 04:21:49 +0800

    debug: register feat

--- services/auth-service/src/app.ts
+++ services/auth-service/src/app.ts
@@ -8,7 +8,7 @@ import authRoutes from './routes/auth.routes';
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(cors({ origin: true }));
 app.use(express.json());
 app.use(
   rateLimit({
